930adf06fc47dbed0119c69bd81497f63acf870e
Nico <nicoabdala722@icloud.com>
2023-10-04 17:25:56 -0300

    agregue boton de switch theme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
@@ -8,21 +8,40 @@ import Qualification from './components/qualification/Qualification';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import ScrollUp from './components/scrollup/ScrollUp';
+import { createContext } from 'react';
+import ReactSwitch from 'react-switch';
+
+
+export const ThemeContext = React.createContext('null');
 
 const App = () => {
+
+  const [theme, setTheme] = useState('dark')
+  const toggleTheme = () =>{
+    setTheme((curr) => (curr === 'light' ? 'dark' : 'light'))
+  }
+
   return (
-    <>
-      <Header />
-      <main className='main'>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      <div className='switch'>
+        <ReactSwitch 
+        onChange={toggleTheme} 
+        checked={theme === 'dark'} 
+        offHandleColor='#1212121' 
+        checkedIcon=''
+        uncheckedIcon=''/>          
+      </div>
+      <main className='main' id={theme}>
+        <Header />
         <Home />
         <About />
         <Skills />
         <Qualification />
         <Contact />
-      </main>
         <Footer />
         <ScrollUp />
-    </>
+      </main>
+    </ThemeContext.Provider>
   )
 }
 
